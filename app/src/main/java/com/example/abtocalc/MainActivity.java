6efac9baf9d9cf7c1c20786a4d045e909bc54478
Dimon-PC <Dimon-PC@DIMON-ADMIN>
2024-04-03 03:55:41 +0500

    добавил вкладку

--- app/src/main/java/com/example/abtocalc/MainActivity.java
+++ app/src/main/java/com/example/abtocalc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.abtocalc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+
+        });}
+
+        public void startNewActivity(View v){
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        }
     }
-}
